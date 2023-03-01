2c23dcd0121fc96373bc65458250e9b07bd890ef
Pedro Henrique <119066915+Pedrocoro0601@users.noreply.github.com>
2023-03-01 14:44:43 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+var inputTarefa = document.getElementById("input-tarefa")
+var btnaddtarefa = document.getElementById("btn-add-tarefa")
+var listatarefa = document.getElementById("lista-tarefas")
+var idtarefa = 0;
+
+
+var audio = new Audio('audio/mario.mp3');
+setTimeout(toca, 2000)
+function toca(){
+    audio.play()
+}
+
+var botao = new Audio("audio/moeda.mp3");
+
+
+
+
+
+btnaddtarefa.addEventListener("click", ()=>{
+    if (inputTarefa.value != ""){
+        idtarefa++;
+        var li = document.createElement("li");
+        li.id = idtarefa;
+        li.innerHTML = `<span>${inputTarefa.value}</span> <a href="#" onclick="removertarefa(event)"><img class="lixeira-icone" name=${idtarefa} src="imagens/lixeira2.png"></a>`;
+        listatarefa.appendChild(li);
+        inputTarefa.value = "";
+        botao.play();
+    }
+    else{
+        alert("digite uma tarefa")
+    }
+
+});
+
+function removertarefa(event){
+    var elementoParaRemover = document.getElementById(event.target.name)
+    elementoParaRemover.remove();
+}
+
